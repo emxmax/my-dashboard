9ac81299307b3b5d6fe885290e71045d0166179c
Max Carrasco <max.carrasco.h@gmail.com>
2025-09-14 23:57:42 -0500

    first commit

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,11 @@
+import { CartCounter } from "@/shoping-cart/components/CartCounter";
+
+export default function CounterPage() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter />
+    </div>
+  );
+}
---
+++ src/pokemons/index.ts
@@ -0,0 +1,6 @@
+import { PokemonGrid } from './components/PokemonGrid';
+import { PokemonCard } from './components/PokemonCard';
+
+
+export type {PokemonsResponse} from './interfaces/pokemons-response';
+export type {SimplePokemon} from './interfaces/simple-pokemon';
