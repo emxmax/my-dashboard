8aa5b3d7ce1f44ec05d70714bdc82958037ef58d
Max Carrasco <max.carrasco.h@gmail.com>
2025-09-17 22:19:03 -0500

    api next count

--- src/shoping-cart/components/CartCounter.tsx
+++ src/shoping-cart/components/CartCounter.tsx
@@ -7,14 +7,29 @@ interface Props{
     value?: number;
 }
 
+export interface CounterResponse {
+    count: number;
+}
+
+const getApiCounter = async(): Promise<CounterResponse> => {
+    const data = await fetch('/api/counter').then(res => res.json());
+    return data;
+}
+
 export const CartCounter = ({value = 0}: Props) => {
 
     const count = useAppSelector(state => state.counter.count);
     const dispatch = useAppDispatch();
+
     //const [count, setCount] = useState(5);
-    useEffect(()=> {
+    /*useEffect(()=> {
         dispatch(initCounterState(value));
-    }, [dispatch, value])
+    }, [dispatch, value])*/
+
+    useEffect(()=> {
+        getApiCounter()
+            .then(({count}) => dispatch(initCounterState(count)));
+    }, [dispatch])
 
     return (
         <>
